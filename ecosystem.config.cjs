149253e1a036d333d9f6d395ee99e14994b1eef7
Krittin Chayasophat <krittin.ch2543@gmail.com>
2026-05-24 04:17:15 +0700

    fix: force pm2 fork mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: "index.js",
       cwd: __dirname,
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
